34d848eb8ff9a353d2703dd66d0fd95eb944942d
CollinWuY <collinwu.y@gmail.com>
2020-09-28 14:03:25 +0000

    Fixed error messages and invaild fields

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import random
 
 app = Flask(__name__)
 app.secret_key = b"#1234567#7654321"
+
 database = {}
 with open('customers.json') as fp:
     database = json.load(fp)
@@ -22,7 +23,7 @@ def show_customers():
 
 @app.route('/customers/add')
 def show_add_customers():
-    return render_template('add_customer.template.html')
+    return render_template('add_customer.template.html', old_values={}, errors={})
 
 
 @app.route('/customers/add', methods=["POST"])
@@ -31,27 +32,48 @@ def process_add_customers():
     last_name = request.form.get('last_name')
     email = request.form.get('email')
 
+    # use dictionary to store error messages
+    errors = {}
+
+    # check firstname provided
+    if not first_name:
+        errors["first_name"] = "Please provide a valid first name"
+    if not last_name:
+        errors["last_name"] = "Please provide a valid last name"
+    if not email:
+        errors["email"] = "Please provide a valid email"
+    if '@' not in email:
+        errors["email"] = "Wrong Email Format"
+
     if 'can_send' in request.form:
         can_send = True
     else:
         can_send = False
 
-    new_customer = {
-        "id": random.randint(1000, 9999),
-        "first_name": first_name,
-        "last_name": last_name,
-        "email": email,
-        "can_send": can_send
-    }
+    if len(errors) == 0:
+        new_customer = {
+            "id": random.randint(1000, 9999),
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "can_send": can_send
+        }
 
-    database.append(new_customer)
+        database.append(new_customer)
 
-    with open('customers.json', 'w') as fp:
-        json.dump(database, fp)
-    flash(
-        f"The customer with the name {new_customer['first_name']} {new_customer['last_name']}"
-        f" has been created successfully")
-    return redirect(url_for('show_customers'))
+        with open('customers.json', 'w') as fp:
+            json.dump(database, fp)
+
+        flash(
+            f"The customer with the name {new_customer['first_name']} {new_customer['last_name']}"
+            f" has been created successfully")
+
+        return redirect(url_for('show_customers'))
+    else:
+        for key, value in errors.items():
+            flash(value, "error")
+
+        return render_template("add_customer.template.html", old_values=request.form, errors=errors)
 
 
 @app.route('/customers/<int:customer_id>/edit')
@@ -75,23 +97,38 @@ def process_edit_customer(customer_id):
             customer_to_edit = each_customer
             break
 
+    errors = {}
+    if not request.form.get('first_name'):
+        errors["first_name"] = "Please provide a valid first name"
+    if not request.form.get('last_name'):
+        errors["last_name"] = "Please provide a valid last name"
+    if not request.form.get('email'):
+        errors["email"] = "Please provide a valid email"
+    if '@' not in request.form.get('email'):
+        errors["email"] = "Wrong Email Format"
+
     if customer_to_edit:
-        customer_to_edit["first_name"] = request.form.get("first_name")
-        customer_to_edit["last_name"] = request.form.get("last_name")
-        customer_to_edit["email"] = request.form.get("email")
+        if len(errors) == 0:
+            customer_to_edit["first_name"] = request.form.get("first_name")
+            customer_to_edit["last_name"] = request.form.get("last_name")
+            customer_to_edit["email"] = request.form.get("email")
 
-        if 'can_send' in request.form:
-            customer_to_edit["can_send"] = True
+            if 'can_send' in request.form:
+                customer_to_edit["can_send"] = True
+            else:
+                customer_to_edit["can_send"] = False
+
+            with open('customers.json', 'w') as fp:
+                json.dump(database, fp)
+            flash(
+                f"The customer with the name {customer_to_edit['first_name']} {customer_to_edit['last_name']}"
+                f" has been EDITED successfully")
+            return redirect(url_for("show_customers"))
         else:
-            customer_to_edit["can_send"] = False
-
-        with open('customers.json', 'w') as fp:
-            json.dump(database, fp)
-        flash(
-            f"The customer with the name {customer_to_edit['first_name']} {customer_to_edit['last_name']}"
-            f" has been EDITED successfully")
-        return redirect(url_for("show_customers"))
+            return render_template('edit_customer.template.html', customer=request.form, errors=errors)
     else:
+        for key, value in errors.items():
+            flash(value, "error")
         return f"The customer with the id of {customer_id} is not found"
 
 
